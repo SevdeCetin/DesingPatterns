56add16cf21de761944c96751aa5c161ddd55ef9
Sevda Çetin <sevdesarecetin@gmail.com>
2023-10-16 15:35:57 +0300

    Add project files.

---
+++ BaseProject/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(email);
+            if (hasUser == null) return View();
+
+            var signInResult = await _signManager.PasswordSignInAsync(hasUser,password,true,false);
+
+            if (!signInResult.Succeeded) return View();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await _signManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
